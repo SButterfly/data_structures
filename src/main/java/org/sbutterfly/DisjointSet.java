1a1bf2e616449302fd47b4c6568b58e22f433451
Sergei Ermakov <sergeiermakof@gmail.com>
2022-06-08 18:54:36 +0300

    Disjoint set

--- src/main/java/org/sbutterfly/DisjointSet.java
+++ src/main/java/org/sbutterfly/DisjointSet.java
@@ -1,2 +1,32 @@
-package org.sbutterfly;public class DisjointSet {
+package org.sbutterfly;
+
+/**
+ * DisjoinSet is a classic data structure to union numbers in groups.
+ */
+public class DisjointSet {
+
+    private final int[] set;
+
+    public DisjointSet(int n) {
+        this.set = new int[n];
+        for (int i = 0; i < n;i++) {
+            this.set[i] = i;
+        }
+    }
+
+    public int getGroup(int i) {
+        if (this.set[i] != i) {
+            this.set[i] = getGroup(this.set[i]);
+        }
+        return this.set[i];
+    }
+
+    public void unit(int x, int y) {
+        int a = getGroup(x);
+        int b = getGroup(y);
+
+        if (a != b) {
+            this.set[a] = b;
+        }
+    }
 }
--- src/test/java/org/sbutterfly/DisjointSetTest.java
+++ src/test/java/org/sbutterfly/DisjointSetTest.java
@@ -1,4 +1,33 @@
-import static org.junit.jupiter.api.Assertions.*;
+package org.sbutterfly;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class DisjointSetTest {
 
+    @Test
+    void simple() {
+        var set = new DisjointSet(2);
+        Assertions.assertEquals(0, set.getGroup(0));
+        Assertions.assertEquals(1, set.getGroup(1));
+
+        set.unit(0, 1);
+        Assertions.assertEquals(1, set.getGroup(0));
+        Assertions.assertEquals(1, set.getGroup(1));
+    }
+
+    @Test
+    void testChain() {
+        var set = new DisjointSet(5);
+
+        set.unit(0, 1);
+        set.unit(1, 2);
+        set.unit(3, 4);
+        set.unit(3, 2);
+        Assertions.assertEquals(2, set.getGroup(0));
+        Assertions.assertEquals(2, set.getGroup(1));
+        Assertions.assertEquals(2, set.getGroup(2));
+        Assertions.assertEquals(2, set.getGroup(3));
+        Assertions.assertEquals(2, set.getGroup(4));
+    }
 }
